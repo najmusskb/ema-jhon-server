f55cdba2ce1c6e9b7e43993e766e3a836712dae2
najmusskb <najmussakib173@gmail.com>
2023-01-20 20:27:51 +0600

    pagination done

--- index.js
+++ index.js
@@ -29,13 +29,17 @@ async function run() {
     try {
         const productCollection = client.db('emaJohn').collection('products');
         app.get('/products', async (req, res) => {
+
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            console.log(page, size);
+
+
             const query = {}
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page * size).limit(size).toArray();
             //cursor.limit(10) eita dile data 10 
             //  porjonto dekhabe
-
-
             const count = await productCollection.estimatedDocumentCount();
             res.send({ count, products });
 
